28dbe87f79ce0bd91cb006bca32df47f35259987
shilpasantpure <santpureshilpa@gmail.com>
2020-06-11 01:28:53 +0530

    Updates

---
+++ precache-manifest.1252e5dd846b96fa8bdb8447bbdc1b15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5f1f5e05d35cbcc8ea3eb5e6db88be",
+    "url": "/new-to-do-app/index.html"
+  },
+  {
+    "revision": "de4c7e3b2a0ee62b34e1",
+    "url": "/new-to-do-app/static/css/main.6bebc3d9.chunk.css"
+  },
+  {
+    "revision": "c734dbd0ec0fb5497c3d",
+    "url": "/new-to-do-app/static/js/2.82e7e100.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-to-do-app/static/js/2.82e7e100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4c7e3b2a0ee62b34e1",
+    "url": "/new-to-do-app/static/js/main.81728cfd.chunk.js"
+  },
+  {
+    "revision": "1b311ecdfcd539e97803",
+    "url": "/new-to-do-app/static/js/runtime-main.54e1efc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-to-do-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
